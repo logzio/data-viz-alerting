dbedca2a0f69bec853517374a83f029060a060bd
Jonathan-Eng <jonathan.engel@logz.io>
2024-11-24 18:35:20 +0200

    replace refId, value with metrics and labels

--- templates/default_template.go
+++ templates/default_template.go
@@ -17,8 +17,17 @@ var DefaultTemplateString = `
 {{ define "__subject" }}[{{ .Status | toUpper }}{{ if eq .Status "firing" }}:{{ .Alerts.Firing | len }}{{ if gt (.Alerts.Resolved | len) 0 }}, RESOLVED:{{ .Alerts.Resolved | len }}{{ end }}{{ end }}] {{ .GroupLabels.SortedPairs.Values | join " " }} {{ if gt (len .CommonLabels) (len .GroupLabels) }}({{ with .CommonLabels.Remove .GroupLabels.Names }}{{ .Values | join " " }}{{ end }}){{ end }}{{ end }}
 
 {{ define "__text_values_list" }}{{ if len .Values }}{{ $first := true }}{{ range $refID, $value := .Values -}}
-{{ if $first }}{{ $first = false }}{{ else }}, {{ end }}{{ $refID }}={{ $value }}{{ end -}}
-{{ else }}[no value]{{ end }}{{ end }}
+{{ if $first }}{{ $first = false }}{{ else }}, {{ end }}
+{{ template "__eval_value_text" . }}
+{{ end -}}
+{{ else }}[no value]{{ end }}
+{{ end }}
+
+{{ define "__eval_value_text" }}
+- Metric: {{ if .Metric }}'{{ .Metric }}'{{ else }}[unknown metric]{{ end }}
+  Labels: {{ .Labels }}
+  Value: {{ .Value }}
+{{ end }}
 
 {{ define "__text_alert_list" }}{{ range . }}
 Value: {{ template "__text_values_list" . }}
